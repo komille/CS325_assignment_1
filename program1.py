bb5f1d5345a63b09aaa70f12e82bc4df4bb274d1
komille <155792223+komille@users.noreply.github.com>
2024-09-08 13:21:27 -0500

    Updating program1.py in GitHub

--- program1.py
+++ program1.py
@@ -3,6 +3,7 @@ number = input("Enter a number:\n")
 
 while number.isnumeric()!= True:
     print("That's not a number")
+    print("Try again")
     number = input("Enter a new number:\n")
 
 print(f'Your number that you picked is {number}')
