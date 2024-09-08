aefaee659c2d538b4ea3f30ff9c1dfa1427c61e0
Korey Miller <komille@siue.edu>
2024-09-08 13:10:28 -0500

    updating program1.py in vs

--- program1.py
+++ program1.py
@@ -5,5 +5,5 @@ while number.isnumeric()!= True:
     print("That's not a number")
     number = input("Enter a new number:\n")
 
-print(f'You have chosen the number {number}')
+print(f'Your number that you picked is {number}')
     
